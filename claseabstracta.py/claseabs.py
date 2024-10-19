4ab5c047f8e62171c91def8cc6d47236da529c08
luis-bu <luisbuemaxx@gmail.com>
2024-10-19 11:57:41 -0500

    cositas de la clase de ingles

--- claseabstracta.py/claseabs.py
+++ claseabstracta.py/claseabs.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 class Persona(ABC):
+    #Hacemos que sea una clase de tipo abstracta
     @classmethod
     @abstractmethod
     def __init__(self, nombre, edad, sexo, actividad):
@@ -7,25 +8,40 @@ class Persona(ABC):
         self.edad = edad
         self.sexo = sexo
         self.actividad = actividad
+    #Método abstracto
     @abstractmethod
-    def actividad(self):
+    def hacer_actividad(self):
         pass
     
+    #Método NO abstracto
     def presentarse(self):
-        print(f"Hola mi nombre es {self.nombre}, mi edad es {self.edad} y trabajo de {self.actividad}")
+        print(f"Hola mi nombre es {self.nombre}, mi edad es {self.edad} y me gusta {self.actividad}")
     
+#Estudiante hereda de la clase abstracta
 class Estudiante(Persona):
-    def __init__(self, nombre, edad, sexo, actividad, carrera):
-        super.__init__(nombre, edad, sexo, actividad)
+    def __init__(self, nombre, edad, sexo, actividad):
+        super().__init__(nombre, edad, sexo, actividad)
 
+    def hacer_actividad(self):
+        return f"Estoy estudiando {self.actividad}"
+    
     def actividad(self):
         print(f"Estoy estudiando: {self.actividad}")
         
-class Tranajador(Persona):
+class Trabajador(Persona):
     def __init__(self, nombre, edad, sexo, actividad, salario):
-        super.__init__(nombre, edad, sexo, actividad)
+        super().__init__(nombre, edad, sexo, actividad)
         self.salario=salario
     
-    def actividad(self):
-        print(f"Estoy trabajando de: {self.actividad}")
+    def hacer_actividad(self):   
+        return f"{self.nombre} esta haciendo su trabajo de {self.actividad}"
+    
         
+juan = Trabajador("Juan", 24, "No binario", "Programador", 100000)
+
+juan.presentarse()
+print(juan.hacer_actividad())
+
+pedro = Estudiante("Pedro", 18, "Masculino", "Cantar")
+pedro.presentarse()
+print(pedro.hacer_actividad())
